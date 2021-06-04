c45e7dccf83977538f1eed250ae810e55f3aa539
kethiraj <54640211+kethiraj@users.noreply.github.com>
2021-06-04 22:15:24 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 sed -i -e 's/variable1/'$1'/g' index.html
-sed -i -e 's/variable2/'Build Number is $2'/g' index.html
-sed -i -e 's/variable3/'Job Name is $3'/g' index.html
+sed -i -e 's/variable2/Build Number is '$2'/g' index.html
+sed -i -e 's/variable3/Job Name is '$3'/g' index.html
